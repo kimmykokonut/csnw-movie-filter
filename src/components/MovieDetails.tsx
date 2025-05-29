032d425c1485cce629d97f4a81f34979ac99d606
Kim Robinson <kimmykokonut@gmail.com>
2025-05-29 14:21:17 -0700

    [component] add placeholder data for movie details page

--- src/components/MovieDetails.tsx
+++ src/components/MovieDetails.tsx
@@ -1,3 +1,24 @@
-const MovieDetails = () => <h1>Details Page</h1>;
+const MovieDetails = () => {
+  return (
+    <>
+      <h1>Details Page</h1>
+      <h2>Title</h2>
+      <p>href wrap title</p>
+      <img src="https://picsum.photos/200/300" alt="Movie poster" />
+      <p>extract</p>
+      <p>Year released</p>
+      <ul>
+        <li>Genre 1</li>
+        <li>Genre 2</li>
+      </ul>
+      <p>Cast</p>
+      <ul>
+        <li>Cast 1</li>
+        <li>Cast 2</li>
+        <li>Cast 3</li>
+      </ul>
+    </>
+  );
+};
 
 export default MovieDetails;
