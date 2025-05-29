3fda12edc237aa9ad9d7473f400bf2941bd4ed5c
Kim Robinson <kimmykokonut@gmail.com>
2025-05-29 13:56:16 -0700

    [router] add react router dom to project

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,23 @@
-import './App.css'
-import Footer from './components/Footer'
-import Header from './components/Header'
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import "./App.css";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import MovieDetails from "./components/MovieDetails";
+import MovieList from "./components/MovieList";
 
 function App() {
-
   return (
     <>
-      <Header />
-      <Footer />
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={<MovieList />} />
+          <Route path="/:id" element={<MovieDetails />} />
+        </Routes>
+        <Footer />
+      </Router>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
