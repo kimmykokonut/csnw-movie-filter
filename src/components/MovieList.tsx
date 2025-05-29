d7c749aac7d3d9f3337bfb11e855f1d3bdb5592b
Kim Robinson <kimmykokonut@gmail.com>
2025-05-29 14:22:44 -0700

    [index] render movie component in movielist component

--- src/components/MovieList.tsx
+++ src/components/MovieList.tsx
@@ -1,3 +1,23 @@
-const MovieList = () => <h1>Home Page: Movies List Here</h1>;
+import FilterListIcon from "@mui/icons-material/FilterList";
+import { Button } from "@mui/material";
+import Movie from "./Movie";
 
+const MovieList = () => {
+  return (
+    <>
+      <h1>Movie List</h1>
+      <Button
+        variant="contained"
+        color="secondary"
+        startIcon={<FilterListIcon />}
+      >
+        Filter
+      </Button>
+      <p>(Filter by genre, decade)</p>
+      {/* iterate through movie cards */}
+      <Movie />
+      <Movie />
+    </>
+  );
+};
 export default MovieList;
