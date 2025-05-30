9ee5f3d88fe7d1b6bf7452a0996bdd53d1797ed8
Kim Robinson <kimmykokonut@gmail.com>
2025-05-30 10:24:25 -0700

    [header/footer] clean up code

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,17 +2,27 @@ import { Box } from "@mui/material";
 
 const Footer = () => {
   return (
-    <Box sx={{width: '100%',
-      position: 'fixed',
-      bottom: 0,
-      left: 0,
-      display: 'flex',
-      justifyContent: 'center',
-      alignItems: 'center',
-      py: 1,
-      }} >
-      built by<a href="https://github.com/kimmykokonut" style={{ textDecoration: 'none', color: 'purple' }}> kimmykokonut</a>
+    <Box
+      sx={{
+        width: "100%",
+        position: "fixed",
+        bottom: 0,
+        left: 0,
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        py: 1,
+        bgcolor: "secondary.main",
+      }}
+    >
+      built by&nbsp;
+      <a
+        href="https://github.com/kimmykokonut"
+        style={{ textDecoration: "none", color: "gold" }}
+      >
+        kimmykokonut
+      </a>
     </Box>
-  )
-}
+  );
+};
 export default Footer;
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,21 +1,17 @@
 import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
-import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
 import { Link } from "react-router-dom";
 
 const Header = () => (
   <Box sx={{ flexGrow: 1 }}>
     <AppBar position="fixed" color="secondary">
-      <Toolbar>
-        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-          <Link to="/" style={{ color: "inherit" }}>
-            Film Finder
-          </Link>
-        </Typography>
-      </Toolbar>
+      <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+        <Link to="/" style={{ color: "inherit" }}>
+          Film Finder
+        </Link>
+      </Typography>
     </AppBar>
-    <Toolbar />
   </Box>
 );
 export default Header;
