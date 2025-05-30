16425bd7ee9371dd3d2716d678c5536f72f93aad
Kim Robinson <kimmykokonut@gmail.com>
2025-05-29 19:16:52 -0700

    [index] add useState to hold movie data and useEffect to gather data on page load

--- src/components/MovieList.tsx
+++ src/components/MovieList.tsx
@@ -1,8 +1,22 @@
 import FilterListIcon from "@mui/icons-material/FilterList";
 import { Button } from "@mui/material";
+import { useEffect, useState } from "react";
+import type { MovieAPIInterface } from "../models";
+import { fetchMovies } from "../services/api/movies";
 import Movie from "./Movie";
 
 const MovieList = () => {
+  const [movies, setMovies] = useState<MovieAPIInterface[]>([]);
+
+  useEffect(() => {
+    async function loadMovies() {
+      const data = await fetchMovies();
+      setMovies(data);
+    }
+    loadMovies();
+  }, []);
+  console.log(movies[0]);
+
   return (
     <>
       <h1>Movie List</h1>
