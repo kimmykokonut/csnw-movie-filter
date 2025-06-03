9febd40ecb4ffbeddcb4b466802fee2fe5e8a063
Kim Robinson <kimmykokonut@gmail.com>
2025-06-03 10:22:09 -0700

    [filter-form] refactor form to use MUI checkbox and select

--- src/components/FilterForm.tsx
+++ src/components/FilterForm.tsx
@@ -1,25 +1,41 @@
 import {
+  Box,
   Button,
+  Checkbox,
   DialogActions,
   DialogContent,
   DialogTitle,
+  FormControl,
+  FormControlLabel,
+  FormGroup,
+  InputLabel,
+  MenuItem,
+  Select,
+  type SelectChangeEvent,
 } from "@mui/material";
+import { useState } from "react";
+import type { FilterValues } from "../models";
 
 interface FilterFormProps {
   onClose: () => void;
-  onSubmit: (filters: { decade: string; genres?: string[] }) => void;
+  onSubmit: (filters: FilterValues) => void;
   genres: string[];
 }
 
-const FilterForm = ({ onClose, onSubmit, genres }: FilterFormProps) => {
-  const filterMovies = (formData: FormData) => {
+const FilterForm: React.FC<FilterFormProps> = ({
+  onClose,
+  onSubmit,
+  genres,
+}: FilterFormProps) => {
+  const [selectedDecade, setSelectedDecade] = useState("");
+  const [selectedGenres, setSelectedGenres] = useState<string[]>([]);
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
     const filters: { decade: string; genres?: string[] } = {
-      decade: formData.get("decade") as string,
+      decade: selectedDecade,
     };
 
-    const selectedGenres = genres.filter(
-      (genre) => formData.get(genre) === "on"
-    );
     if (selectedGenres.length > 0) {
       filters.genres = selectedGenres;
     }
@@ -28,43 +44,80 @@ const FilterForm = ({ onClose, onSubmit, genres }: FilterFormProps) => {
     onClose();
   };
 
+  const handleGenreSelect = (genre: string, checked: boolean): void => {
+    setSelectedGenres((prev) =>
+      checked ? [...prev, genre] : prev.filter((g) => g !== genre)
+    );
+  };
+
   return (
     <>
-      <form action={filterMovies}>
+      <form onSubmit={handleSubmit}>
         <DialogTitle id="filter-dialog-title">Filter Movie Results</DialogTitle>
         <DialogContent>
-          <div>
-            <label htmlFor="decade">By Decade:</label>
-            <select name="decade" id="decade" autoFocus>
-              <option value="">All Decades</option>
-              <option value="1900">1900s</option>
-              <option value="1910">1910s</option>
-              <option value="1920">1920s</option>
-              <option value="1930">1930s</option>
-              <option value="1940">1940s</option>
-              <option value="1950">1950s</option>
-              <option value="1960">1960s</option>
-              <option value="1970">1970s</option>
-              <option value="1980">1980s</option>
-              <option value="1990">1990s</option>
-              <option value="2000">2000s</option>
-              <option value="2010">2010s</option>
-              <option value="2020">2020s</option>
-            </select>
-          </div>
-          <div>
-            <p>By Genre</p>
-            <div>
+          <Box sx={{ mb: 1, display: "flex", alignItems: "center", gap: 2 }}>
+            <InputLabel id="decade" sx={{ mb: 1 }}>
+              By Decade:
+            </InputLabel>
+            <FormControl>
+              <Select
+                name="decade"
+                id="decade"
+                autoFocus
+                value={selectedDecade}
+                defaultValue=""
+                displayEmpty
+                onChange={(e: SelectChangeEvent<string>) =>
+                  setSelectedDecade(e.target.value)
+                }
+                size="small"
+              >
+                <MenuItem value="">All Decades</MenuItem>
+                <MenuItem value="1900">1900s</MenuItem>
+                <MenuItem value="1910">1910s</MenuItem>
+                <MenuItem value="1920">1920s</MenuItem>
+                <MenuItem value="1930">1930s</MenuItem>
+                <MenuItem value="1940">1940s</MenuItem>
+                <MenuItem value="1950">1950s</MenuItem>
+                <MenuItem value="1960">1960s</MenuItem>
+                <MenuItem value="1970">1970s</MenuItem>
+                <MenuItem value="1980">1980s</MenuItem>
+                <MenuItem value="1990">1990s</MenuItem>
+                <MenuItem value="2000">2000s</MenuItem>
+                <MenuItem value="2010">2010s</MenuItem>
+                <MenuItem value="2020">2020s</MenuItem>
+              </Select>
+            </FormControl>
+          </Box>
+          <Box sx={{ mb: 1 }}>
+            <InputLabel sx={{ mb: 1 }}>By Genre</InputLabel>
+            <FormGroup
+              sx={{
+                display: "flex",
+                flexDirection: "row",
+                flexWrap: "wrap",
+              }}
+            >
               {genres.map((genre) => (
-                <div key={genre}>
-                  <label htmlFor={genre}>
-                    {genre}
-                    <input type="checkbox" id={genre} name={genre} />
-                  </label>
-                </div>
+                <FormControlLabel
+                  key={genre}
+                  label={genre}
+                  control={
+                    <Checkbox
+                      id={genre}
+                      name={genre}
+                      checked={selectedGenres.includes(genre)}
+                      onChange={(e) =>
+                        handleGenreSelect(genre, e.target.checked)
+                      }
+                      size="small"
+                      color="secondary"
+                    />
+                  }
+                />
               ))}
-            </div>
-          </div>
+            </FormGroup>
+          </Box>
         </DialogContent>
         <DialogActions>
           <Button onClick={onClose}>Cancel</Button>
@@ -78,17 +131,3 @@ const FilterForm = ({ onClose, onSubmit, genres }: FilterFormProps) => {
 };
 
 export default FilterForm;
-
-//mui example
-//   <TextField
-//     autoFocus
-//     required
-//     margin="dense"
-//     id="name"
-//     name="email"
-//     label="Email Address"
-//     type="email"
-//     fullWidth
-//     variant="standard"
-//   />
-// </DialogContent>
